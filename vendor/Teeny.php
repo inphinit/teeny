c12e4295781d7acf1249f402128d629d4b1d0616
Guilherme Nascimento <brcontainer@yahoo.com.br>
2024-05-13 00:21:53 -0300

    Update Teeny.php

--- vendor/Teeny.php
+++ vendor/Teeny.php
@@ -24,8 +24,8 @@ class Teeny
         'alpha' => '[a-zA-Z]+',
         'decimal' => '\d+\.\d+',
         'num' => '\d+',
-        'noslash' => '[^\/]+',
-        'nospace' => '[^\/\s]+',
+        'noslash' => '[^/]+',
+        'nospace' => '[^/\s]+',
         'uuid' => '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}',
         'version' => '\d+\.\d+(\.\d+(-[\da-zA-Z]+(\.[\da-zA-Z]+)*(\+[\da-zA-Z]+(\.[\da-zA-Z]+)*)?)?)?'
     );
